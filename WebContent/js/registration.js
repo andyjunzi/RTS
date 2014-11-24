74467f44f1a55e619d5287d3bddeadf8cbf59efb
jhfan <jhuafan@gmail.com>
2014-11-24 15:19:39 -0500

    15:19 11/24/2014
    Add datepicker

--- WebContent/js/registration.js
+++ WebContent/js/registration.js
@@ -1,8 +1,8 @@
-var app = angular.module('form-example1', []);
+var app = angular.module('form-example1', ['ui.bootstrap']);
 
-app.controller('appCtrl', ['$scope', function($scope) {
+app.controller('appCtrl', function($scope) {
     $scope.master = {};
-
+    
     $scope.update = function(user) {
       $scope.master = angular.copy(user);
     };
@@ -12,7 +12,7 @@ app.controller('appCtrl', ['$scope', function($scope) {
     };
 
     $scope.reset();
-  }]);
+  });
 
 app.directive('username', function($q, $timeout) {
   return {
@@ -45,6 +45,42 @@ app.directive('username', function($q, $timeout) {
     }
   };
 });
+
+app.controller('DatepickerDemoCtrl', function ($scope) {
+	  $scope.today = function() {
+	    $scope.dt = new Date();
+	  };
+	  $scope.today();
+
+	  $scope.clear = function () {
+	    $scope.dt = null;
+	  };
+
+	  // Disable weekend selection
+	  $scope.disabled = function(date, mode) {
+	    return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
+	  };
+
+	  $scope.toggleMin = function() {
+	    $scope.minDate = $scope.minDate ? null : new Date();
+	  };
+	  $scope.toggleMin();
+
+	  $scope.open = function($event) {
+	    $event.preventDefault();
+	    $event.stopPropagation();
+
+	    $scope.opened = true;
+	  };
+
+	  $scope.dateOptions = {
+	    formatYear: 'yy',
+	    startingDay: 1
+	  };
+
+	  $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+	  $scope.format = $scope.formats[0];
+	});
 
 app.controller('modi', function ($scope) {
 	$scope.getItemState = function (item) {
